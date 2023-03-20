bc94bb81cd901566e6a0e287cd4020d0dbf35409
“yongjian.tek” <yongjian.tek@grabtaxi.com>
2023-03-20 15:58:47 +0800

    push diff

--- m5-forecasting/getfiles.sh
+++ m5-forecasting/getfiles.sh
@@ -1,3 +1,4 @@
 kaggle competitions download -c m5-forecasting-accuracy --path ./data
+kaggle competitions download -c m5-forecasting-uncertainty --path ./data
 unzip ./data/m5-forecasting-accuracy.zip -d data
 # rm ./data/m5-forecasting-accuracy.zip
