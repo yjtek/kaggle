049d4e40deca6082e94f816ff8fcd4fe256bfde6
“yongjian.tek” <yongjian.tek@grabtaxi.com>
2023-03-20 16:05:37 +0800

    amend getfiles

--- m5-forecasting/getfiles.sh
+++ m5-forecasting/getfiles.sh
@@ -1,5 +1,7 @@
 kaggle competitions download -c m5-forecasting-accuracy --path ./data
 kaggle competitions download -c m5-forecasting-uncertainty --path ./data
-# unzip ./data/m5-forecasting-accuracy.zip -d data
-# unzip ./data/m5-forecasting-uncertainty.zip -d data
+unzip ./data/m5-forecasting-accuracy.zip -d data
+mv ./data/sample_submission.csv ./data/sample_submission_accuracy.csv 
+unzip ./data/m5-forecasting-uncertainty.zip -d data
+mv ./data/sample_submission.csv ./data/sample_submission_uncertainty.csv 
 # rm ./data/m5-forecasting-accuracy.zip
