d4ba9f3d41ecbe9c33a2bcbf50e1913c7e605d13
yjtek <yoggibear6@gmail.com>
2023-03-20 10:40:46 +0000

    push colab changes

--- m5-forecasting/getfiles.sh
+++ m5-forecasting/getfiles.sh
@@ -1,7 +1,7 @@
-kaggle competitions download -c m5-forecasting-accuracy --path ./data
-kaggle competitions download -c m5-forecasting-uncertainty --path ./data
-unzip ./data/m5-forecasting-accuracy.zip -d data
-mv ./data/sample_submission.csv ./data/sample_submission_accuracy.csv 
-unzip ./data/m5-forecasting-uncertainty.zip -d data
-mv ./data/sample_submission.csv ./data/sample_submission_uncertainty.csv 
+kaggle competitions download -c m5-forecasting-accuracy --path ./m5-forecasting/data
+kaggle competitions download -c m5-forecasting-uncertainty --path ./m5-forecasting/data
+unzip ./m5-forecasting/data/m5-forecasting-accuracy.zip -d ./m5-forecasting/data
+mv ./m5-forecasting/data/sample_submission.csv ./m5-forecasting/data/sample_submission_accuracy.csv 
+unzip ./m5-forecasting/data/m5-forecasting-uncertainty.zip -d ./m5-forecasting/data
+mv ./m5-forecasting/data/sample_submission.csv ./m5-forecasting/data/sample_submission_uncertainty.csv
 # rm ./data/m5-forecasting-accuracy.zip
