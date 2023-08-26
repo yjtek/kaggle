29278bde6614b4b38f500ac78eb657e012b244b3
“yongjian.tek” <yongjian.tek@grabtaxi.com>
2023-08-26 21:48:02 +0800

    sync

--- 1. Titanic/utils/data/__init__.py
+++ 1. Titanic/utils/data/__init__.py
@@ -2,25 +2,40 @@ from dataclasses import dataclass
 from ..baseclass import Dataset
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from loguru import logger
 
 @dataclass
 class TitanicDataset(Dataset):
     
     df: pd.DataFrame
     outcome_col: str
+    retain_outcome_col: bool = False
+    test_size: float = 0.3
+    random_state: int = 123
 
     def __post_init__(self):
         if self.outcome_col in self.df.columns:
-            self.X = (
-                self.df.drop(self.outcome_col)
-            ).to_pandas()
-            self.y = (
-                self.df.select(self.outcome_col)
-            ).to_pandas()
-            self.split_train_test()
+            if not self.retain_outcome_col:
+                self.X = (
+                    self.df.drop(self.outcome_col)
+                ).to_pandas()
+                self.y = (
+                    self.df.select(self.outcome_col)
+                ).to_pandas()
+                self.split_train_test()
+            else:
+                logger.warning(f'Retaining outcome column `{self.outcome_col}` in X. You **must** drop this before training')
+                self.X = (
+                    self.df
+                ).to_pandas()
+                self.y = (
+                    self.df.select(self.outcome_col)
+                ).to_pandas()
+                self.split_train_test()
+
         else:
             self.X = self.df.to_pandas()
             self.y = None
 
     def split_train_test(self):
-        self.xtrain, self.xtest, self.ytrain, self.ytest = train_test_split(self.X, self.y, test_size=0.3)
+        self.xtrain, self.xtest, self.ytrain, self.ytest = train_test_split(self.X, self.y, test_size=self.test_size, random_state=self.random_state)
--- 1. Titanic/utils/transformers/__init__.py
+++ 1. Titanic/utils/transformers/__init__.py
@@ -3,6 +3,10 @@ from sklearn.base import BaseEstimator, TransformerMixin
 import polars as pl
 import numpy as np
 import re
+from typing import Any
+from loguru import logger
+from catboost import CatBoostClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 @PolarsCompatibleTransformer
 class MakeNameFeatures(BaseEstimator, TransformerMixin):
@@ -62,7 +66,7 @@ class TransformColToCategorical(BaseEstimator, TransformerMixin):
 
 @PolarsCompatibleTransformer
 class TransformStringColToNumeric(BaseEstimator, TransformerMixin):
-    def __init__(self, column_name: str, levels_threshold: int = 20, fill_nulls: str = -1, replace_original: bool = True):
+    def __init__(self, column_name: str, levels_threshold: int = 20, fill_nulls: Any = -1, replace_original: bool = True):
         self.levels_threshold = levels_threshold
         self.column_name = column_name
         self.fill_nulls = fill_nulls
@@ -96,11 +100,12 @@ class TransformStringColToNumeric(BaseEstimator, TransformerMixin):
 
 @PolarsCompatibleTransformer
 class TransformColToBins:
-    def __init__(self, column_name: str, bin_count: int = 10, fill_nulls=-1, replace_original: bool = True):
+    def __init__(self, column_name: str, bin_count: int = 10, fill_nulls=-1, replace_original: bool = True, return_numeric=True):
         self.column_name: str = column_name
         self.bin_count: int = bin_count
         self.fill_nulls: str = fill_nulls
         self.replace_original = replace_original
+        self.return_numeric = return_numeric
         self.new_column_name = column_name if replace_original else f'{column_name}_binned'
     
     def fit(self, X, y=None):
@@ -119,10 +124,20 @@ class TransformColToBins:
                 .fill_nan(self.fill_nulls)
                 .fill_null(self.fill_nulls)
                 .alias(f'{self.new_column_name}')
-                .cast(pl.Utf8)
-                .cast(pl.Categorical)
             )
         )
+
+        if self.return_numeric:
+            X_transformed = (
+                X_transformed
+                .with_columns(pl.col(self.new_column_name).cast(pl.Int64).alias(self.new_column_name))
+            )
+        else:
+            X_transformed = (
+                X_transformed
+                .with_columns(pl.col(self.new_column_name).cast(pl.Utf8).cast(pl.Categorical).alias(self.new_column_name))
+            )
+        
         return X_transformed
 
 @PolarsCompatibleTransformer
@@ -132,6 +147,7 @@ class AddFamilyUnitID(BaseEstimator, TransformerMixin):
     
     def fit(self, X, y=None):
         self.rank_on_col = X.columns[0]
+        
         return self
     
     def transform(self, X, y=None):
@@ -139,58 +155,183 @@ class AddFamilyUnitID(BaseEstimator, TransformerMixin):
         X_transformed = (
             X
             .with_columns(
-                pl.when(
-                    pl.col(self.rank_on_col)
-                    .rank()
-                    .over(self.family_group) == 1
-                ).then(
-                    pl.lit(1)
-                ).otherwise(
-                    pl.lit(0)
-                ).alias(f"groupid_{'_'.join(self.family_group)}")
-            )
-            .sort(self.family_group)
-            .with_columns(
-                pl.cumsum(f"groupid_{'_'.join(self.family_group)}").alias(f"groupid_{'_'.join(self.family_group)}")
+                pl.concat_str(self.family_group).rank(method='dense').alias(f"groupid_{'_'.join(self.family_group)}")
             )
         )
         return X_transformed
 
 @PolarsCompatibleTransformer
 class AddSurvivalRate(BaseEstimator, TransformerMixin):
-    def __init__(self, survival_rate_group: list[str]):
-        self.survival_rate_group = survival_rate_group
+    def __init__(self, family_group: list[str]):
+        self.family_group = family_group
+        self.family_group_string = '_'.join(self.family_group)
+        self.groupid_col = f"groupid_{self.family_group_string}"
 
-    def fit(self, X, y):
-        self.survivalrate_by_group_id = (
-            pl.concat([X,y], how='horizontal')
-            .groupby(self.survival_rate_group)
+    def fit(self, X, y=None):
+        self.survivalrate_by_family_group = (
+            X
+            .groupby(self.groupid_col)
             .agg(
-                pl.mean('Survived').alias(f'survival_rate_{"_".join(self.survival_rate_group)}')
+                pl.sum('Survived').alias(f'survivedsum_{self.family_group_string}'),
+                pl.count('Survived').alias(f'survivedcount_{self.family_group_string}'),
+                #pl.mean('Survived').alias(f'survivedrate_{self.family_group_string}')
             )
         )
+
+        self.survivalrate_by_family_group
+
         return self
 
     def transform(self, X, y=None):
         X_transformed = (
             X
-            .join(self.survivalrate_by_group_id, on=self.survival_rate_group, how='left')
+            .join(self.survivalrate_by_family_group, on=self.groupid_col, how='left')
+            .with_columns(
+                pl.when(
+                    pl.col(f'survivedcount_{self.family_group_string}') > 1
+                ).then(
+                    ((pl.col(f'survivedsum_{self.family_group_string}')) / 
+                    (pl.col(f'survivedcount_{self.family_group_string}')))
+                ).otherwise(
+                    pl.lit(-1)
+                ).alias(f'survivalrate_{self.family_group_string}')
+            )
+            .drop([f'survivedsum_{self.family_group_string}', f'survivedcount_{self.family_group_string}'])
         )
         return X_transformed
 
 @PolarsCompatibleTransformer
 class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, cols_to_drop: list[str], regex_to_drop: str):
+    def __init__(self, cols_to_drop: list[str] = [], regex_to_drop: str = '', drop_strings: bool = True):
         self.cols_to_drop: list[str] = cols_to_drop
         self.regex_to_drop: str = regex_to_drop
+        self.drop_strings: bool = drop_strings
     
     def fit(self, X, y=None):
-        self.drop_cols_with_regex = [col for col in X.columns if re.search(self.regex_to_drop, col) is not None]
+        self.drop_cols_with_regex = [col for col in X.columns if re.search(self.regex_to_drop, col) is not None] if self.regex_to_drop != '' else []
+        
+        self.drop_cols_with_string = [colname for colname, coltype in zip(X.columns, X.dtypes) if coltype == pl.Utf8] if self.drop_strings else []
+        # self.drop_cols_with_string = [key for key,value in dict(X.dtypes).items() if value == np.dtypes.ObjectDType]
+        logger.info(f'String drop cols: {self.drop_cols_with_string}')
+
+        self.final_drop_cols = list(set([colname for colname in self.cols_to_drop + self.drop_cols_with_regex + self.drop_cols_with_string if colname in X.columns]))
+
+        logger.info(f'Specified drop cols: {self.cols_to_drop}')
+        logger.info(f'Regex drop cols: {self.drop_cols_with_regex}')
+        logger.info(f'String drop cols: {self.drop_cols_with_string}')
+        logger.info(f'Dropping cols: {self.final_drop_cols}')
+
+        return self
+
+    def transform(self, X, y=None):
+        only_drop_present_cols = [col for col in X.columns if col in self.final_drop_cols]
+        X_transformed = (
+            X
+            .drop(*only_drop_present_cols)
+        )
+        return X_transformed
+
+@PolarsCompatibleTransformer
+class FillNull(BaseEstimator, TransformerMixin):
+    def __init__(self, null_value: float):
+        self.null_value = null_value
+    
+    def fit(self, X, y=None):
         return self
 
     def transform(self, X, y=None):
         X_transformed = (
             X
-            .drop(list(set(self.cols_to_drop + self.drop_cols_with_regex)))
+            .fill_null(self.null_value)
+            .fill_nan(self.null_value)
+        )
+        return X_transformed
+
+@PolarsCompatibleTransformer
+class AddRandomColumn(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.random_state = 123
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        np.random.seed(self.random_state)
+        X_transformed = (
+            X
+            .with_columns(random = pl.lit(np.random.rand(X.height)))
+        )
+        return X_transformed
+
+@PolarsCompatibleTransformer
+class CleanCabin(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        ...
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_transformed = (
+            X
+            .with_columns(
+                pl.col("Cabin").fill_null('').alias('Cabin'),
+                pl.col("Cabin").str.slice(0,1).alias('CabinFirstLetter')
+            )
+        )
+        return X_transformed
+
+@PolarsCompatibleTransformer
+class CleanHonorific(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        ...
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X_transformed = (
+            X
+            # .with_columns(
+            #     pl.when(
+            #         pl.col("Honorific").is_in([''])
+            #     ).then(
+
+            #     )
+            # )
+        )
+        return X_transformed
+
+class PipelineCompatiableCatBoostClassifier(CatBoostClassifier):
+    # def __init__(self, **kwargs):
+    #     ...
+
+    def fit(self, X, y=None, **kwargs):
+        self.cat_features = [colname for colname, coltype in dict(X.dtypes).items() if type(coltype) == pd.core.dtypes.dtypes.CategoricalDtype]
+        model = (
+            super()
+            .fit(X, y=y, cat_features=self.cat_features, **kwargs)
+        )
+        return model
+
+@PolarsCompatibleTransformer
+class CleanAge(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.rf = RandomForestRegressor()
+    
+    def fit(self, X, y=None):
+        X_without_missing = X.filter(pl.col('Age').is_not_null())
+        outcome = X_without_missing.select('Age').to_pandas()
+        features = X_without_missing.select('SibSp', 'Parch', 'Pclass', 'Fare').to_pandas()
+        self.rf.fit(features, outcome)
+        return self
+
+    def transform(self, X, y=None):
+
+        age_preds = self.rf.predict(X.select('SibSp', 'Parch', 'Pclass', 'Fare').to_pandas())
+        X_transformed = (
+            X
+            .with_columns(pl.Series(age_preds).alias('AgePred'))
+            .with_columns(pl.coalesce(pl.col(['Age', 'AgePred'])).alias('Age'))
         )
         return X_transformed
